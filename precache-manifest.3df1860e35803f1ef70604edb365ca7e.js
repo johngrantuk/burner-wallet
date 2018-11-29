c359584fb928b01d2e5a6056c4955959e6b22a8e
John Mac <johngrantuk@googlemail.com>
2018-11-29 17:17:21 +0000

    Updates

---
+++ precache-manifest.3df1860e35803f1ef70604edb365ca7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "09f3696e5f7be73687fd",
+    "url": "/xDai/static/js/runtime~main.09f3696e.js"
+  },
+  {
+    "revision": "b563fd8d12b66c228ddc",
+    "url": "/xDai/static/js/main.b563fd8d.chunk.js"
+  },
+  {
+    "revision": "ad599c98c221de1690b9",
+    "url": "/xDai/static/js/1.ad599c98.chunk.js"
+  },
+  {
+    "revision": "b563fd8d12b66c228ddc",
+    "url": "/xDai/static/css/main.225a830b.chunk.css"
+  },
+  {
+    "revision": "ad599c98c221de1690b9",
+    "url": "/xDai/static/css/1.9ce7f623.chunk.css"
+  },
+  {
+    "revision": "33ac2e3a627741fc3e094cd85a09c476",
+    "url": "/xDai/index.html"
+  }
+];
