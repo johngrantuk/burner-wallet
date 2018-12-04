d51256a187c06c14a8d61e6eb774fa9b0ea17184
John Mac <johngrantuk@googlemail.com>
2018-12-04 16:46:09 +0000

    Scanner test app.

---
+++ src/components/ScannerTest.jsx
@@ -0,0 +1,108 @@
+import React, { Component } from "react";
+import QrReader from "react-qr-reader";
+
+export default class ScannerTest extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      delay: 500,
+      isScanning: true,
+      errorMessage: '',
+      isError: false,
+      result: 'Please Scan A QR'
+    };
+
+    this.handleError = this.handleError.bind(this);
+    this.openImageDialog = this.openImageDialog.bind(this);
+  }
+
+  stopRecording = () => this.setState({ delay: false });
+
+  handleScan = data => {
+
+    if (data) {
+
+      if(!this.state.isError){
+        console.log('Stopping')
+        this.stopRecording();
+        this.setState({
+          errorMessage: 'Hopefully It Worked Ok? If not please update Issue - Thanks',
+          isScanning: false
+        });
+      }
+
+      console.log('QR Data: ' + data)
+      this.setState({ result: 'QR Data: ' + data });
+    }
+  };
+
+  handleError(err){
+    this.setState({
+      result: 'Please upload a QR Code',
+      errorMessage: err.toString(),
+      isError: true
+    });
+
+  };
+
+  openImageDialog() {
+      this.refs.qrReader1.openImageDialog()
+  }
+
+  render() {
+
+    let display = '';
+    let errorMessage = this.state.errorMessage;
+
+    if(this.state.isError){
+      display = (
+        <div>
+        <h1>Scanner Test</h1>
+        <div>
+          <p>{this.state.result}</p>
+          <input type="button" value="Submit QR Code" onClick={this.openImageDialog} />
+          <p>Reason You Couldn't Scan: { errorMessage }</p>
+          <QrReader
+            ref="qrReader1"
+            delay={this.state.delay}
+            onError={this.handleError}
+            onScan={this.handleScan}
+            style={{ width: "30%", height: "30%" }}
+            legacyMode={true}
+          />
+        </div>
+        </div>
+      )
+    }else{
+
+      let scanner = '';
+
+      if(this.state.isScanning === true){
+          scanner = <QrReader
+            delay={this.state.delay}
+            onError={this.handleError}
+            onScan={this.handleScan}
+            style={{ width: "30%", height: "30%" }}
+          />
+      }
+
+      display = (
+        <div>
+        <h1>Scanner Test</h1>
+        <div>
+          <p>{this.state.result}</p>
+          <p>{ errorMessage }</p>
+          { scanner }
+        </div>
+        </div>
+      )
+    }
+
+    return (
+      <div style={{ padding: 30 }}>
+      { display }
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ScannerTest from './components/ScannerTest';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ScannerTest />, document.getElementById('root'));
