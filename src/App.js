df13627ac85fae2fc7b21576769c2079d13f280d
John Mac <johngrantuk@googlemail.com>
2018-12-04 11:21:46 +0000

    Updating.

--- src/App.js
+++ src/App.js
@@ -361,7 +361,6 @@ class App extends Component {
 
     return (
       <div className="App">
-        <h1>TEST</h1>
         <Dapparatus
           config={{
             DEBUG:false,
---
+++ src/AppLinkTest.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+    return (
+      <>
+        <h1>test</h1>
+        <div>And this is me giving the info.</div>
+      </>
+    );
+  }
+}
+
+export default App;
